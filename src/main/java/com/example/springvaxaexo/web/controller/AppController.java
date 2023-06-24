43a1711b6fc963e6389cee7c8a9ebd11a7e32183
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-06-24 17:22:19 +0200

    exo28 done

--- src/main/java/com/example/springvaxaexo/web/controller/AppController.java
+++ src/main/java/com/example/springvaxaexo/web/controller/AppController.java
@@ -5,10 +5,7 @@ import com.example.springvaxaexo.business.data.MonstreInfo;
 import com.example.springvaxaexo.business.logic.Joueur;
 import com.example.springvaxaexo.business.logic.Poring;
 import com.example.springvaxaexo.web.dto.NewGameResponseDTO;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -19,5 +16,11 @@ public class AppController {
         Joueur joueur = nomJoueur == null ? new Joueur() : new Joueur(nomJoueur);
         Poring poring = new Poring();
         return new NewGameResponseDTO((JoueurInfo) joueur.getData(), (MonstreInfo) poring.getData());
+    }
+
+    @GetMapping("/newPlayer")
+    public JoueurInfo newPlayer(@RequestParam(value = "nomJoueur", required = false) String nomJoueur) {
+        Joueur joueur = nomJoueur == null ? new Joueur() : new Joueur(nomJoueur);
+        return (JoueurInfo) joueur.getData();
     }
 }
